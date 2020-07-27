8d9f914fe39d5cc3a8b50263422b5fdd7aebaad2
mintchococookie <parksungju90@gmail.com>
2020-07-27 17:31:54 -0500

    Chapter2

---
+++ Exercise Files/Chapter 02/.metadata/.plugins/org.eclipse.core.resources/.history/19/40e4428358d0001a192f8b5558b6e580
@@ -0,0 +1,9 @@
+package com.toddperkins;
+
+public class Review {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- Exercise Files/Chapter 02/Review/src/com/toddperkins/Review.java
+++ Exercise Files/Chapter 02/Review/src/com/toddperkins/Review.java
@@ -3,7 +3,8 @@ package com.toddperkins;
 public class Review {
 
 	public static void main(String[] args) {
-		
+		String myName = "Todd";
+		System.out.println(myName);
 	}
 
 }
