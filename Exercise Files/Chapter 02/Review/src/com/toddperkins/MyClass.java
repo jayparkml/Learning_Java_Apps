84b2bd0294a35fb62921e4b1e4417f68209c10c2
mintchococookie <parksungju90@gmail.com>
2020-07-28 17:31:40 -0500

    Chapter2

---
+++ Exercise Files/Chapter 02/Review/src/com/toddperkins/MyClass.java
@@ -0,0 +1,17 @@
+package com.toddperkins;
+
+public class MyClass {
+
+	
+	public int myInt = 5;
+	
+	MyClass() {
+		System.out.println("constructor");
+	}
+	
+	
+	void myMethod() {
+		System.out.println("my method");
+	}
+	
+}
